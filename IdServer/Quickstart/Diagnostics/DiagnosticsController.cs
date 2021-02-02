3c10e260fc2d1c0205fccf5afc041d9bfd243307
Bito <bitofvg@gmail.com>
2021-02-02 14:47:03 +0100

    Part 05: Id Server Fix Diagnostics page

--- IdServer/Quickstart/Diagnostics/DiagnosticsController.cs
+++ IdServer/Quickstart/Diagnostics/DiagnosticsController.cs
@@ -7,23 +7,27 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
 
-namespace IdentityServerHost.Quickstart.UI
-{
-    [SecurityHeaders]
-    [Authorize]
-    public class DiagnosticsController : Controller
-    {
-        public async Task<IActionResult> Index()
-        {
-            var localAddresses = new string[] { "127.0.0.1", "::1", HttpContext.Connection.LocalIpAddress.ToString() };
-            if (!localAddresses.Contains(HttpContext.Connection.RemoteIpAddress.ToString()))
-            {
-                return NotFound();
-            }
-
-            var model = new DiagnosticsViewModel(await HttpContext.AuthenticateAsync());
-            return View(model);
-        }
+namespace IdentityServerHost.Quickstart.UI {
+  [SecurityHeaders]
+  [Authorize]
+  public class DiagnosticsController : Controller {
+    private IHostEnvironment env;
+    public DiagnosticsController(IHostEnvironment hostEnvironment) {
+      env = hostEnvironment;
     }
+
+    public async Task<IActionResult> Index() {
+      //var localAddresses = new string[] { "127.0.0.1", "::1", HttpContext.Connection.LocalIpAddress.ToString() };
+      //if (!localAddresses.Contains(HttpContext.Connection.RemoteIpAddress.ToString()))
+      if (!env.IsDevelopment()) {
+
+        return NotFound();
+      }
+
+      var model = new DiagnosticsViewModel(await HttpContext.AuthenticateAsync());
+      return View(model);
+    }
+  }
 }
